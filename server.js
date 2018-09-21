b17d832e1cac4aa75a09425ce07a5c7bbd23b6dd
lenny <lenmorld@gmail.com>
2018-09-21 01:38:04 -0400

    hello world html

--- server.js
+++ server.js
@@ -4,19 +4,10 @@ var port = 4000;
 
 var server = http.createServer(function (req, res) { // Callback function
     // Response header
-    res.writeHead(200, { "Content-Type": "application/json" });
-    
-    // JSON object
-    var song = {
-        id: 12345,
-        favorite: false,
-        title: "Hello World",
-        artist: "Node programmer",
-        album: "Node EP"
-    };
+    res.writeHead(200, { "Content-Type": "text/html" });
 
-    // send JSON response to client
-    res.end(JSON.stringify(song));      // JSON.stringify({a: 1}) -> '{"a":1}'
+    // send HTML response to client
+    res.end("<h1>Hello World</h1>");
 });
 
 server.listen(port, function () { // Callback function
