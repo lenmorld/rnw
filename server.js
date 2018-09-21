3dc0d1be61503d5aae61c6ad94a607fa89dd94ed
lenny <lenmorld@gmail.com>
2018-09-21 01:15:10 -0400

    *formatting*

--- server.js
+++ server.js
@@ -2,14 +2,13 @@
 var http = require('http');
 var port = 4000;
 
-
-var server = http.createServer(function (req, res) {    // Callback function
+var server = http.createServer(function (req, res) { // Callback function
     // Response header
     res.writeHead(200, { "Content-Type": "text/plain" });
     // send response
     res.end("Hello World\n");
 });
 
-server.listen(port, function () {   // Callback function
+server.listen(port, function () { // Callback function
     console.log("Starting server at " + port);
 });
