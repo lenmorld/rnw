3901cb059680269a9d347534f0f077f35eb26bfa
lenny <lenmorld@gmail.com>
2018-09-23 19:46:16 -0400

    render Item attributes

--- app/Item.jsx
+++ app/Item.jsx
@@ -3,11 +3,20 @@ import React from 'react';
 class Item extends React.Component {
     render() {
         var item = this.props.item;
-        console.log("Item:", item);
-
-        return(
-            <div>
-                ...item...
+        // console.log("Item:", item);
+        
+        return (
+            <div className="item">
+                <div className="left">
+                    <iframe src={"https://open.spotify.com/embed/track/" + item.id}
+                        width="80" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media">
+                    </iframe>
+                </div>
+                <div className="right">
+                    <div className="title">{item.title}</div>
+                    <div className="artist">{item.artist}</div>
+                    <div className="album">{item.album}</div>
+                </div>
             </div>
         );
     }
