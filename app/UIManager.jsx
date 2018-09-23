8e0e61fedfbd4502793a80587570f95ff88121dc
lenny <lenmorld@gmail.com>
2018-09-23 17:05:40 -0400

    add UIManager component

---
+++ app/UIManager.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import data from './data';
+
+console.log(data);
+
+class UIManager extends React.Component {
+    render() {
+        return(
+            <div>List goes here...</div>
+        );
+    }
+}
+
+export default UIManager;
