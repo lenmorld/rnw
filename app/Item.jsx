854378d070d751923e904d29f8f91e8c812759a2
lenny <lenmorld@gmail.com>
2018-09-23 19:36:39 -0400

    log Item props at Imte

--- app/Item.jsx
+++ app/Item.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 class Item extends React.Component {
     render() {
+        var item = this.props.item;
+        console.log("Item:", item);
+
         return(
             <div>
                 ...item...
