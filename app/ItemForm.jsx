35cf972c88c58f4b615984cbfe79373c95e465e4
lenny <lenmorld@gmail.com>
2018-09-26 02:10:47 -0400

    event and event handlers

--- app/ItemForm.jsx
+++ app/ItemForm.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
 class ItemForm extends React.Component {
+
+    hideForm() {
+        console.log("hide form");
+    }
+
+    onSubmitForm(event) {
+        console.log("form submitted");
+    }
+
+
+    onChangeInput(event) {
+        console.log("input changed");
+    }
+
     render() {
 
         var item = this.props.item;
@@ -13,32 +27,36 @@ class ItemForm extends React.Component {
             <div>
                 <form>
                     <div className="close_form">
-                        <span>[🗙]</span>
+                        <span onClick={this.hideForm}>[🗙]</span>
                     </div>
                     <h3>Create a new item</h3>
                     <p>
                         <label>ID:</label>
                         <input name="id" 
+                               onChange={(event) => this.onChangeInput(event)}  
                                value={item.id} />
                     </p>
                     <p>
                         <label>Title:</label>
                         <input name="title" 
+                               onChange={(event) => this.onChangeInput(event)}  
                                value={item.title} />
                     </p>
                     <p>
                         <label>Artist:</label>
                         <input name="artist" 
+                               onChange={(event) => this.onChangeInput(event)}  
                                value={item.artist} />
                     </p>
                     <p>
                         <label>Album:</label>
                         <input name="album" 
+                               onChange={(event) => this.onChangeInput(event)}  
                                value={item.album} />
                     </p>
 
                     <div className="create">
-                        <button>
+                        <button onClick={(event) => this.onSubmitForm(event) }>
                             CREATE
                         </button>
                     </div>
