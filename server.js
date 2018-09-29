74d7096fef1a7048e35bd49f7d1c3ae079485d7e
lenny <lenmorld@gmail.com>
2018-09-29 15:57:05 -0400

    dont close db, cant connect after

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ mongo_db.init_db(db_connection_url).then(function(db_instance) {
     // TEST
     db_collection.find().toArray(function(err, result) {
         console.log({ "list": result});
-        db_instance.close();
     });
 
     // run server and pass db collection reference for executing commands
