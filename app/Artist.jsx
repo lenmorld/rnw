026790f4915e91d88e8116e3a540589e52895a06
lenny <lenmorld@gmail.com>
2018-10-24 21:18:17 -0400

    basic routes

---
+++ app/Artist.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Artist = () => (
+    <div>
+        <h2>Artist</h2>
+    </div>
+);
+
+export default Artist;
--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import UIManager from './UIManager';
 import Header from './Header';
+import Track from './Track';
+import Artist from './Artist';
 
 class App extends React.Component {
     render() {
@@ -12,7 +14,18 @@ class App extends React.Component {
             <BrowserRouter>
                 <div>
                     <Header />
-                    <UIManager />
+
+                    {/* sample links to take us to the route */}
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/artist">Artist</Link></li>
+                        <li><Link to="/track">Track</Link></li>
+                    </ul>
+
+                    {/* match route to React component */}
+                    <Route exact path="/" component={UIManager} />
+                    <Route exact path="/artist" component={Artist} />
+                    <Route exact path="/track" component={Track} />
                 </div>
             </BrowserRouter>
         );
